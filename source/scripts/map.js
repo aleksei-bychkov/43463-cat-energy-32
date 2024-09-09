c5e55ea489e5de1f5af503394fe9767b99c3bf86
Aleksei Bychkov <nwsm@bk.ru>
2024-09-09 18:11:55 +0300

    Добавляет исправления

--- source/scripts/map.js
+++ source/scripts/map.js
@@ -1,42 +1,30 @@
-// eslint-disable-next-line no-undef
-ymaps.ready(init);
-function init () {
-  // Создание экземпляра карты и его привязка к контейнеру с
-  // заданным id ("map").
-  // eslint-disable-next-line no-undef
-  const myMap = new ymaps.Map('ymap', {
-    // При инициализации карты обязательно нужно указать
-    // её центр и коэффициент масштабирования.
-    center: [59.938631, 30.323037], // Санк-Петербург
-    zoom: 16,
-    controls: []
-  });
+document.addEventListener('DOMContentLoaded', () => {
 
-  // eslint-disable-next-line no-undef
-  myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-    hintContent: 'Cat Energy'
-  }, {
-    // Опции.
-    // Необходимо указать данный тип макета.
-    iconLayout: 'default#image',
-    // Своё изображение иконки метки.
-    iconImageHref: '../images/map-pin@2x.png',
-    // Размеры метки.
-    iconImageSize: [90, 80],
-    // Смещение левого верхнего угла иконки относительно
-    // её "ножки" (точки привязки).
-    iconImageOffset: [-45, -75]
-  });
-
-  myMap.geoObjects
-    // eslint-disable-next-line no-undef
-    .add(myPlacemark);
-  //Сдвигаем центр карты для размера от 1440px
+  const ymaps = window.ymaps;
   const mq = window.matchMedia('(min-width: 1440px)');
+  ymaps.ready(() => {
+    const myMap = new ymaps.Map('ymap', {
+        center: [59.938679, 30.3230044],
+        zoom: 16,
+        controls: []
+      }),
 
-  if (mq.matches) {
-    myMap.setCenter([59.938703, 30.318006]);
-  } else {
-    myMap.setCenter([59.938631, 30.323037]);
-  }
-}
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+        iconLayout: 'default#image',
+        iconImageHref: '../images/map-pin@2x.png',
+        iconImageSize: [90, 80],
+        iconImageOffset: [-45, -75]
+      });
+
+
+    if (mq.matches) {
+      myMap.setCenter([59.938703, 30.318006]);
+    } else {
+      myMap.setCenter([59.938631, 30.323037]);
+    }
+
+
+    myMap.geoObjects.add(myPlacemark);
+  });
+
+});
